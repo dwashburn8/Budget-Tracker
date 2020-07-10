7de91c4e053fc77f75362623256b1e5f28d5568f
Dallas Washburn <soldbydw@gmail.com>
2020-07-09 18:13:19 -0700

    fixed mongo

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
+  Transaction.find({})
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
